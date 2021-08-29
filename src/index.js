6aad795ca867df8d10190df1c8c7155f583e2845
Jaime Zamora <kratov@protonmail.com>
2021-08-29 18:50:58 -0500

    logging emmit errors

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const event = req.body;
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
-
-    res.send({status: "OK"});
+    try {
+        await axios.post("http://localhost:4000/events", event);
+        await axios.post("http://localhost:4001/events", event);
+        await axios.post("http://localhost:4002/events", event);
+        res.send({status: "OK"});
+    }catch (e) {
+        console.error(`${e.message}`.bgRed.black);
+    }
 })
 
 app.listen(4005, () => {
