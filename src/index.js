38f6213ecf551edb8602eaa6cf49e413d3244eeb
Jaime Zamora <kratov@protonmail.com>
2021-08-29 23:06:02 -0500

    Sync events and event bus bug

--- src/index.js
+++ src/index.js
@@ -7,18 +7,36 @@ require('colors');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.post('/events', async (req, res) => {
     const event = req.body;
+    events.push(event);
     try {
         await axios.post("http://localhost:4000/events", event);
-        await axios.post("http://localhost:4001/events", event);
-        await axios.post("http://localhost:4002/events", event);
-        await axios.post("http://localhost:4003/events", event);
-        res.send({status: "OK"});
     }catch (e) {
         console.error(`${e.message}`.bgRed.black);
     }
+    try {
+        await axios.post("http://localhost:4001/events", event);
+    }catch (e) {
+        console.error(`${e.message}`.bgRed.black);
+    }
+    try {
+        await axios.post("http://localhost:4002/events", event);
+    }catch (e) {
+        console.error(`${e.message}`.bgRed.black);
+    }
+    try {
+        await axios.post("http://localhost:4003/events", event);
+    }catch (e) {
+        console.error(`${e.message}`.bgRed.black);
+    }
+    res.send({status: "OK"});
 })
 
 app.listen(4005, () => {
